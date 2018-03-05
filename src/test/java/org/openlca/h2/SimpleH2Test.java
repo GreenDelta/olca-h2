29a0dfb60891908dcae4cd5e8a22e54c2ca7958d
msrocka <michael.srocka@gmail.com>
2018-03-05 11:00:54 +0100

    test hikary with h2

--- src/test/java/org/openlca/h2/SimpleH2Test.java
+++ src/test/java/org/openlca/h2/SimpleH2Test.java
@@ -1,5 +1,6 @@
 package org.openlca.h2;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,25 +11,45 @@ import java.sql.Statement;
 
 public class SimpleH2Test {
 
+	private String url = "jdbc:h2:mem:testdb" + System.nanoTime();
+
 	@Test
-	public void testSimpleTable() throws Exception {
-		String url = "jdbc:h2:mem:testdb" + System.nanoTime() + ";MODE=MySQL";
+	public void testSimpleConnection() throws Exception {
 		Connection con = DriverManager.getConnection(url, "sa", "");
 		Assert.assertTrue(con.isValid(200));
+		setupTable(con);
+		checkValues(con);
+		con.close();
+		Assert.assertTrue(con.isClosed());
+	}
 
-		// create a table
+	@Test
+	public void testWithConnectionPool() throws Exception {
+		HikariDataSource pool = new HikariDataSource();
+		pool.setJdbcUrl(url);
+		pool.setUsername("sa");
+		pool.setPassword("");
+		try (Connection con = pool.getConnection()) {
+			setupTable(con);
+		}
+		try (Connection con = pool.getConnection()) {
+			checkValues(con);
+		}
+		pool.close();
+	}
+
+	private void setupTable(Connection con) throws Exception {
 		Statement stmt = con.createStatement();
 		stmt.execute("CREATE TABLE test_values ( TEST_VALUES VARCHAR(255) )");
 		stmt.close();
-
-		// insert values
 		stmt = con.createStatement();
 		stmt.execute("INSERT INTO TEST_VALUES (test_values) " +
 				"VALUES ('abc'), ('def'), ('hji')");
 		stmt.close();
+	}
 
-		// query values
-		stmt = con.createStatement();
+	private void checkValues(Connection con) throws Exception {
+		Statement stmt = con.createStatement();
 		ResultSet rs = stmt.executeQuery("select * from TEST_VALUES");
 		String[] values = {"abc", "def", "hji"};
 		int i = 0;
@@ -36,10 +57,9 @@ public class SimpleH2Test {
 			Assert.assertEquals(values[i], rs.getString(1));
 			i++;
 		}
-		Assert.assertEquals(3, i);
-
+		rs.close();
 		con.close();
-		Assert.assertTrue(con.isClosed());
+		Assert.assertEquals(3, i);
 	}
 
 }
